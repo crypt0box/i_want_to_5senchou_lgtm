33f5b1f1fe282174dee556fbd50dcd36bb2e881e
crypt0box <pray.happy76@gmail.com>
2020-11-18 19:43:57 +0900

    UPDATE: Get QiitaTrendArticle by axios close #1

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <display-qiita-trend></display-qiita-trend>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import DisplayQiitaTrend from './components/DisplayQiitaTrend.vue';
 
 export default Vue.extend({
   name: 'App',
   components: {
-    HelloWorld
+    DisplayQiitaTrend,
   }
 });
 </script>
---
+++ src/components/DisplayQiitaTrend.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div v-for="article in articleList" :key="article.index">
+      {{ article }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import axios from 'axios';
+
+export default Vue.extend({
+  name: 'DisplayQiitaTrend',
+  data() {
+    return {
+      articleList: Array,
+    }
+  },
+  created() {
+    axios
+      .get('https://qiita-api.netlify.app/.netlify/functions/trend')
+      .then(response => this.articleList = response.data)
+      .catch(error => {
+        console.log('httpリクエストにs失敗しました', error)
+      });
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
