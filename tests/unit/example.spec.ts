9ba4f7306537f1d13d038962adc7d7785bfa31c7
crypt0box <pray.happy76@gmail.com>
2020-12-03 20:03:38 +0900

    ADD: add test code

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,10 +1,10 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import DisplayQiitaTrend from '@/components/DisplayQiitaTrend.vue'
 
-describe('HelloWorld.vue', () => {
+describe('DisplayQiitaTrend.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(DisplayQiitaTrend, {
       propsData: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
